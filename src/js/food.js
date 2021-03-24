d0855a07608737efef329003ca1e0762a4392136
Xoom7573 <66717268+Xoom7573@users.noreply.github.com>
2021-03-24 17:52:26 +0100

    init

---
+++ src/js/food.js
@@ -0,0 +1,28 @@
+import {onSnake, expandSnake} from './snake.js'
+import { randomGridPos } from './grid.js'
+
+let food = getRandomFoodPos();
+const EXPANSION_RATE = 1;
+
+export function update() {
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE);
+        food = getRandomFoodPos();
+    }
+}
+
+export function draw(gameDisplay) {
+    const foodElement = document.createElement('div')
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food')
+    gameDisplay.appendChild(foodElement)
+}
+
+function getRandomFoodPos() {
+    let newFoodPostion
+    while(newFoodPostion == null || onSnake(newFoodPostion)){
+        newFoodPostion = randomGridPos();
+    }
+    return newFoodPostion;
+}
---
+++ src/js/snakegame.js
@@ -0,0 +1,46 @@
+import { update as updateSnake, draw as drawSnake, snakeHead, snakeIntersection, SNAKE_SPEED } from './snake.js'
+import { update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+// ___Variables___
+let lastRenderTime = 0;
+let gameOver = false;
+const gameDisplay = document.getElementById('game-display');
+
+// ___Game functions___
+
+function main (currentTime) {
+
+    if (gameOver){
+        if (confirm('You lost. Press ok to restart.')){
+            window.location = '/'
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+    
+    lastRenderTime = currentTime;
+    update();
+    draw();
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake();
+    updateFood();
+    checkDeath();
+}
+
+function draw() {
+    gameDisplay.innerHTML = '';
+    drawSnake(gameDisplay);
+    drawFood(gameDisplay);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(snakeHead()) || snakeIntersection();
+}
