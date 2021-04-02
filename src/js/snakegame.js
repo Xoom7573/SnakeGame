81c7b4dba201fade00ebcdb8fb6dc4bfd8091810
Xoom7573 <66717268+Xoom7573@users.noreply.github.com>
2021-04-02 11:59:05 +0200

    Update snakegame.js

--- src/js/snakegame.js
+++ src/js/snakegame.js
@@ -15,7 +15,7 @@ function main (currentTime) {
 
     if (gameOver){
         if (confirm('You lost. Press ok to restart.')){
-            window.location = '/'
+            window.location = '/SnakeGame'
         }
         return
     }
